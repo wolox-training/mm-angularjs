4d7fc64f03aeda97e38fe1d24bf54d31e960099e
MMarussi <jmmarussi32@gmail.com>
2018-04-27 09:23:39 -0300

    Functional Components

--- tictactoe/src/app/screens/Game/components/Square/index.js
+++ tictactoe/src/app/screens/Game/components/Square/index.js
@@ -1,17 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import style from './styles.scss';
 
-class Square extends Component {
-  state = { value: null };
-  render() {
-    return (
-      <button className={style.square} onClick={() => this.props.onClick()}>
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button className={style.square} onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 Square.propTypes = {
