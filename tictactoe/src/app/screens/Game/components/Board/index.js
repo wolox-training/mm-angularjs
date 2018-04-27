153d0f55d504224e7bac2e658258ca210414b886
MMarussi <jmmarussi32@gmail.com>
2018-04-27 09:33:36 -0300

    Declaring a Winner

--- tictactoe/src/app/screens/Game/components/Board/index.js
+++ tictactoe/src/app/screens/Game/components/Board/index.js
@@ -11,6 +11,9 @@ class Board extends Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares,
@@ -20,7 +23,13 @@ class Board extends Component {
 
   renderSquare = i => <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
   render() {
-    const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = `Winner: ${winner}`;
+    } else {
+      status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+    }
 
     return (
       <Fragment>
@@ -43,6 +52,26 @@ class Board extends Component {
       </Fragment>
     );
   }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i += 1) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 export default Board;
