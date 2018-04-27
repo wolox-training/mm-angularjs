d2c8d7ae4c45583dbaa7bc0a0e809e893ac75c6e
MMarussi <jmmarussi32@gmail.com>
2018-04-27 09:28:10 -0300

    Taking Turns

--- tictactoe/src/app/screens/Game/components/Board/index.js
+++ tictactoe/src/app/screens/Game/components/Board/index.js
@@ -4,17 +4,23 @@ import Square from './../Square/';
 import style from './styles.scss';
 
 class Board extends Component {
-  state = { squares: Array(9).fill(null) };
+  state = {
+    squares: Array(9).fill(null),
+    xIsNext: true
+  };
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({ squares });
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares,
+      xIsNext: !this.state.xIsNext
+    });
   }
 
   renderSquare = i => <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
   render() {
-    const status = 'Next player: X';
+    const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
 
     return (
       <Fragment>
